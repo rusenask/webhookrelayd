816d21baa6e34866470a362e647c3db1cbbcf60d
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-12-29 20:30:47 +0200

    initial relay implementation

---
+++ relay/relay.go
@@ -0,0 +1,77 @@
+package relay
+
+import (
+	"bytes"
+	"net/http"
+
+	"github.com/hashicorp/go-retryablehttp"
+
+	pb "github.com/rusenask/webhookrelayd/grpc/webhook"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+// Relayer - relayer interface
+type Relayer interface {
+	Relay(wh *pb.WebhookRequest) error
+}
+
+// DefaultRelayer - default 'last mile' webhook relayer
+type DefaultRelayer struct {
+	// client *http.Client
+
+	rClient *retryablehttp.Client
+
+	// maximum number of retries that this relayer should try
+	// before giving up.
+	retries int
+
+	// backoff strategy in seconds
+	backoff int
+}
+
+// Opts - configuration
+type Opts struct {
+	Retries int
+}
+
+// NewDefaultRelayer - create an instance of default relayer
+func NewDefaultRelayer(opts *Opts) *DefaultRelayer {
+	client := retryablehttp.NewClient()
+
+	client.RetryMax = opts.Retries
+
+	return &DefaultRelayer{rClient: client}
+}
+
+func getHeaders(wh *pb.WebhookRequest) http.Header {
+	headers := make(map[string][]string)
+	for k, v := range wh.Request.Header.Headers {
+		headers[k] = v.Values
+	}
+
+	return headers
+}
+
+// Relay - relaying incomming webhook to original destination
+func (r *DefaultRelayer) Relay(wh *pb.WebhookRequest) error {
+	req, err := retryablehttp.NewRequest(wh.Request.Method, wh.Request.Destination, bytes.NewReader(wh.Request.Body))
+
+	// adding headers
+	req.Header = getHeaders(wh)
+
+	resp, err := r.rClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode > 399 {
+		log.WithFields(log.Fields{
+			"status_code": resp.StatusCode,
+			"destination": wh.Request.Destination,
+			"method":      wh.Request.Method,
+		}).Warn("relayer: unexpected status code")
+	}
+
+	return nil
+}
